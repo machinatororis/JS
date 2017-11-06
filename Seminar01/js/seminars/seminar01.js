cd4ab11c3e8cb684a0a897601734eda2008988d9
machinatororis <machinatororis@gmail.com>
2017-11-06 17:17:13 +0200

    sprinners

--- Seminar01/js/seminars/seminar01.js
+++ Seminar01/js/seminars/seminar01.js
@@ -24,10 +24,47 @@ var __;
             //$frect ( 0, 0, w, h, "#000");
             __.$line_cap("round");
             // $sline(w / 2, 10, w / 2, h - 10, "#ff0000", l * 10);
-            __.$fcircle(w / 2, h / 4, h / 6, "#fcc");
-            __.$fcircle(w * 0.3, h / 1.7, h / 6, "#fcc");
-            __.$fcircle(w * 0.7, h / 1.7, h / 6, "#fcc");
-            __.$fcircle(w / 2, h / 2, h / 6, "#fcc");
+            __.$fcircle(w / 2, h / 4, h / 6, "#c7eb5e");
+            __.$scircle(w / 2, h / 4, h / 6, "#895ea7", l * 10);
+            __.$fcircle(w * 0.3, h / 1.7, h / 6, "#c7eb5e");
+            __.$scircle(w * 0.3, h / 1.7, h / 6, "#895ea7", l * 10);
+            __.$fcircle(w * 0.7, h / 1.7, h / 6, "#c7eb5e");
+            __.$scircle(w * 0.7, h / 1.7, h / 6, "#895ea7", l * 10);
+            __.$fcircle(w / 2, h / 2, h / 6, "#c7eb5e");
+            __.$fcircle(w / 2, h / 2, 50, "#895ea7");
+        });
+        __.$$draw("spinner@spinner1", __.round(__.WIDTH / 2, 1), __.round(__.HEIGHT / 2, 1), 0, 1, 0.5, 0.5, function (w, h, l) {
+            //$frect ( 0, 0, w, h, "#000");
+            __.$line_cap("round");
+            //$sline(w / 2, 10, w / 2, h - 10, "#ff0000", l * 10);
+            //$fcircle(w / 2 , h / 4, h / 6, "#fcc");
+            //$fcircle(w * 0.3, h / 1.7, h / 6, "#fcc");
+            //$fcircle(w * 0.7, h / 1.7, h / 6, "#fcc");
+            __.$fcircle(w / 2, h / 2, h / 6 + 10, "#17a5a3"); //центральный круг
+            __.$sline(w / 2, h / 2, w / 2, 80, "#17a5a3", l * 120);
+            __.$fcircle(w / 2, 80, h / 6, "#17a5a3");
+            __.$fcircle(w / 2, 80, 30, "#deeb7b"); //центр 
+            __.$sline(w / 2, h / 2, 120, h / 2 + 80, "#17a5a3", l * 120);
+            __.$fcircle(120, h / 2 + 80, h / 6, "#17a5a3");
+            __.$fcircle(120, h / 2 + 80, 30, "#deeb7b"); //центр 
+            __.$sline(w / 2, h / 2, 360, h / 2 + 70, "#17a5a3", l * 120);
+            __.$fcircle(360, h / 2 + 70, h / 6, "#17a5a3");
+            __.$fcircle(360, h / 2 + 70, 30, "#deeb7b"); //центр
+            __.$fcircle(w / 2, h / 2, 50, "#deeb7b"); //центр 
+        });
+        __.$$draw("spinner@spinner2", __.round(__.WIDTH / 2, 1), __.round(__.HEIGHT / 2, 1), 0, 1, 0.5, 0.5, function (w, h, l) {
+            //$frect ( 0, 0, w, h, "#000");
+            //$line_cap("round");
+            __.$fcircle(w / 2, h / 2, h / 6, "#da2865");
+            __.$frect(w / 2 + 10, h / 2 + 10, 100, 100, "#da2865");
+            __.$frect(w / 2 + 50, h / 2 + 50, 50, 50, "#98e2e1"); //центр
+            __.$frect(w / 2 - 110, h / 2 - 110, 100, 100, "#da2865");
+            __.$frect(w / 2 - 100, h / 2 - 100, 50, 50, "#98e2e1"); //центр
+            __.$frect(w / 2 - 110, h / 2 + 10, 100, 100, "#da2865");
+            __.$frect(w / 2 - 100, h / 2 + 50, 50, 50, "#98e2e1"); //центр
+            __.$frect(w / 2 + 10, h / 2 - 110, 100, 100, "#da2865");
+            __.$frect(w / 2 + 50, h / 2 - 100, 50, 50, "#98e2e1"); //центр
+            __.$fcircle(w / 2, h / 2, 50, "#98e2e1");
         });
         __.$$apply();
     });
@@ -50,7 +87,7 @@ var __;
      */
     __.app_update(function () {
         // if (DEBUGGING) log("app_update() " + TICKS + " " + TIME);
-        r += 20;
+        r += 10;
     });
     /**
      *  Обработка тачей.
@@ -73,7 +110,9 @@ var __;
      */
     __.app_draw(function () {
         // if (DEBUGGING) log("app_draw() ");
-        __.sprite_draw("spinner@spinner0", __.WIDTH / 2, __.HEIGHT / 2, undefined, undefined, 1, 1, r);
+        __.sprite_draw("spinner@spinner0", __.WIDTH / 4, __.HEIGHT / 4, undefined, undefined, 1, 1, r);
+        __.sprite_draw("spinner@spinner1", __.WIDTH / 2 + 200, __.HEIGHT / 2 - 200, undefined, undefined, 1, 1, r);
+        __.sprite_draw("spinner@spinner2", __.WIDTH / 2 - 200, __.HEIGHT / 2 + 200, undefined, undefined, 1, 1, r);
     });
 })(__ || (__ = {}));
 //# sourceMappingURL=seminar01.js.map
--- Seminar01/js/seminars/seminar01.ts
+++ Seminar01/js/seminars/seminar01.ts
@@ -30,11 +30,49 @@ module __
             //$frect ( 0, 0, w, h, "#000");
             $line_cap("round");
            // $sline(w / 2, 10, w / 2, h - 10, "#ff0000", l * 10);
-            $fcircle(w / 2, h / 4, h / 6, "#fcc");
-            $fcircle(w * 0.3, h / 1.7, h / 6, "#fcc");
-            $fcircle(w * 0.7, h / 1.7, h / 6, "#fcc");
-            $fcircle(w / 2, h / 2 , h / 6, "#fcc");
+            $fcircle(w / 2 , h / 4, h / 6, "#c7eb5e");
+            $scircle(w / 2 , h / 4, h / 6, "#895ea7", l * 10);
+            $fcircle(w * 0.3, h / 1.7, h / 6, "#c7eb5e");
+            $scircle(w * 0.3, h / 1.7, h / 6, "#895ea7", l * 10);
+            $fcircle(w * 0.7, h / 1.7, h / 6, "#c7eb5e");
+            $scircle(w * 0.7, h / 1.7, h / 6, "#895ea7", l * 10);
+            $fcircle(w / 2, h / 2 , h / 6, "#c7eb5e");
+            $fcircle(w / 2, h / 2 , 50, "#895ea7");
         }); 
+        $$draw("spinner@spinner1", round(WIDTH / 2, 1), round(HEIGHT / 2, 1), 0, 1, 0.5, 0.5, (w: pixels, h: pixels, l: pixels) => {
+            //$frect ( 0, 0, w, h, "#000");
+            $line_cap("round");
+            //$sline(w / 2, 10, w / 2, h - 10, "#ff0000", l * 10);
+            //$fcircle(w / 2 , h / 4, h / 6, "#fcc");
+            //$fcircle(w * 0.3, h / 1.7, h / 6, "#fcc");
+            //$fcircle(w * 0.7, h / 1.7, h / 6, "#fcc");
+            $fcircle(w / 2, h / 2 , h / 6 + 10, "#17a5a3"); //центральный круг
+            $sline(w / 2, h / 2, w / 2, 80, "#17a5a3", l * 120);
+            $fcircle(w / 2, 80 , h / 6, "#17a5a3");
+            $fcircle(w / 2, 80 , 30, "#deeb7b"); //центр 
+            $sline(w / 2, h / 2, 120, h / 2 + 80, "#17a5a3", l * 120);
+            $fcircle(120, h / 2 + 80 , h / 6, "#17a5a3");
+            $fcircle(120, h / 2 + 80 , 30, "#deeb7b"); //центр 
+            $sline(w / 2, h / 2, 360, h / 2 + 70, "#17a5a3", l * 120);
+            $fcircle(360, h / 2 + 70 , h / 6, "#17a5a3");
+            $fcircle(360, h / 2 + 70 , 30, "#deeb7b"); //центр
+            $fcircle(w / 2, h / 2, 50, "#deeb7b"); //центр 
+            
+        });
+        $$draw("spinner@spinner2", round(WIDTH / 2, 1), round(HEIGHT / 2, 1), 0, 1, 0.5, 0.5, (w: pixels, h: pixels, l: pixels) => {
+            //$frect ( 0, 0, w, h, "#000");
+            //$line_cap("round");
+            $fcircle(w / 2, h / 2 , h / 6, "#da2865");
+            $frect( w / 2 + 10, h / 2 + 10, 100, 100, "#da2865");
+            $frect( w / 2 + 50, h / 2 + 50, 50, 50, "#98e2e1"); //центр
+            $frect( w / 2 - 110, h / 2 - 110, 100, 100, "#da2865");
+            $frect( w / 2 - 100, h / 2 - 100, 50, 50, "#98e2e1"); //центр
+            $frect( w / 2 - 110, h / 2 + 10, 100, 100, "#da2865");
+            $frect( w / 2 - 100, h / 2 + 50, 50, 50, "#98e2e1"); //центр
+            $frect( w / 2 + 10, h / 2 - 110, 100, 100, "#da2865");
+            $frect( w / 2 + 50, h / 2 - 100, 50, 50, "#98e2e1"); //центр
+            $fcircle(w / 2, h / 2 , 50, "#98e2e1");
+        });
 
         $$apply();
 
@@ -66,7 +104,7 @@ module __
     app_update(() =>
     {
         // if (DEBUGGING) log("app_update() " + TICKS + " " + TIME);
-        r += 20;
+        r += 10;
     });
 
 
@@ -95,7 +133,9 @@ module __
     app_draw(() :void =>
     {
         // if (DEBUGGING) log("app_draw() ");
-        sprite_draw("spinner@spinner0", WIDTH / 2, HEIGHT / 2, undefined, undefined, 1, 1, r);
+        sprite_draw("spinner@spinner0", WIDTH / 4, HEIGHT / 4, undefined, undefined, 1, 1, r);
+        sprite_draw("spinner@spinner1", WIDTH / 2 + 200, HEIGHT / 2 - 200, undefined, undefined, 1, 1, r);
+        sprite_draw("spinner@spinner2", WIDTH / 2 - 200, HEIGHT / 2 + 200, undefined, undefined, 1, 1, r);
 
     });
 
